5cb4e9b7759c99f2ef46bb5b8726fc85f6b7b4b6
Jeremy Schneider <jeremy.schneider@gmail.com>
2018-06-16 20:18:35 -0700

    Fixing style issue

--- www.grasshopperfinance.dev.cc/wp-content/themes/wp-bootstrap-4-child/functions.php
+++ www.grasshopperfinance.dev.cc/wp-content/themes/wp-bootstrap-4-child/functions.php
@@ -1,14 +1,17 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'ritz-style'; // This is 'ritz-style' for the ritz theme.
+    $parent_style = 'wp-bootstrap-4-style'; // This is 'ritz-style' for the ritz theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( "grasshopper-style", "/wp-includes/grasshopper/css/grasshopper.css");
+
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
 }
 
 function load_scripts() {
