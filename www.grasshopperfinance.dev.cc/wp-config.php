3971698337c07611b28c3ac407c32e1affad65c3
Jeremy Schneider <jeremy.schneider@gmail.com>
2019-01-10 18:16:19 -0800

    Updating config to force https

--- www.grasshopperfinance.dev.cc/wp-config.php
+++ www.grasshopperfinance.dev.cc/wp-config.php
@@ -11,6 +11,10 @@
 if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
 
+
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+    $_SERVER['HTTPS']='on';
+
 #Define Sprimary url
 #Don't uncomment these as it messes up local development. URL is fine online
 #define( 'WP_HOME', 'http://www.personalfinanceclub.com' );
